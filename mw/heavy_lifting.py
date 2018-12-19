7011eaef538be2b8a84b89ddc6a26d6e692ebcb7
johnedstone <johnedstone@gmail.com>
2018-12-18 21:22:50 -0500

    Working! Next BS

--- mw/heavy_lifting.py
+++ mw/heavy_lifting.py
@@ -18,63 +18,59 @@ from django.conf import settings
 def heavy_lifting(submit_id='unknown_submit_id', fancy=True):
     ''' Sample:
     with io.StringIO() as fh:
-        fh.write('Submit ID; {}'.format('woo hoo'))
-        path = default_storage.save('results/query_results.html', ContentFile(fh.getvalue().encode('utf-8')))
+        fh.write('Submit ID; {}'.format(submit_id))
+        path = default_storage.save('results.html', ContentFile(fh.getvalue().encode('utf-8')))
     or
 
-    fh = default_storage.open('results.html', 'w') # can not get this from settings for some reason
+    fh = default_storage.open('results.html', 'w')
     fh.write(submit_id)
     fh.close()
     '''
 
-    fh = default_storage.open('results.html', 'w') # can not get this from settings for some reason
-    fh.write(submit_id)
-    fh.close()
-    return
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--window-size=1280x1696')
+    chrome_options.add_argument('--user-data-dir=/tmp/user-data')
+    chrome_options.add_argument('--hide-scrollbars')
+    chrome_options.add_argument('--enable-logging')
+    chrome_options.add_argument('--log-level=0')
+    chrome_options.add_argument('--v={}'.format(settings.CHROM_VERBOSITY))
+    chrome_options.add_argument('--single-process')
+    chrome_options.add_argument('--data-path=/tmp/data-path')
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--homedir=/tmp')
+    chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
+    chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+    chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
+    executable_path = os.getcwd() + '/bin/chromedriver'
 
-#    #with io.StringIO() as fh:
-#    #    fh.write('{}.\n'.format('woo hoo accordian'))
-#    #    path = default_storage.save('results/query_results.html', ContentFile(fh.getvalue().encode('utf-8')))
-#
-#
-#    chrome_options = Options()
-#    chrome_options.add_argument('--headless')
-#    chrome_options.add_argument('--no-sandbox')
-#    chrome_options.add_argument('--disable-gpu')
-#    chrome_options.add_argument('--window-size=1280x1696')
-#    chrome_options.add_argument('--user-data-dir=/tmp/user-data')
-#    chrome_options.add_argument('--hide-scrollbars')
-#    chrome_options.add_argument('--enable-logging')
-#    chrome_options.add_argument('--log-level=0')
-#    chrome_options.add_argument('--v={}'.format(settings.CHROM_VERBOSITY))
-#    chrome_options.add_argument('--single-process')
-#    chrome_options.add_argument('--data-path=/tmp/data-path')
-#    chrome_options.add_argument('--ignore-certificate-errors')
-#    chrome_options.add_argument('--homedir=/tmp')
-#    chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
-#    chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
-#    chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
-#    executable_path = os.getcwd() + '/bin/chromedriver'
-#
-#    driver = webdriver.Chrome(executable_path=executable_path,
-#        chrome_options=chrome_options)
-#        
-#    if 1 == 1:
-#    #try:
-##        driver.get(settings.URL)
-##        driver.find_element_by_id('username').send_keys(settings.USER)
-##        driver.find_element_by_id ('password').send_keys(settings.PW)
-##        driver.find_element_by_css_selector('button.loginoperation').click()
-##        driver.get(settings.DASHBOARD)
-##        driver.find_element_by_css_selector('a[href="{}"]'.format(settings.ORDERS)).click()
-##        # driver.get(ORDERS) # This could replace the line above
-##        #print(driver.page_source)
-##
-##        data = driver.page_source
-##        
-##        soup = BS(data,'lxml')
-##        table = soup.find_all('table')[0]
-##
+    driver = webdriver.Chrome(executable_path=executable_path,
+        chrome_options=chrome_options)
+        
+    #try:
+    if 1 == 1:
+        driver.get(settings.URL)
+        driver.find_element_by_id('username').send_keys(settings.USER)
+        driver.find_element_by_id ('password').send_keys(settings.PW)
+        driver.find_element_by_css_selector('button.loginoperation').click()
+        driver.get(settings.DASHBOARD)
+        driver.find_element_by_css_selector('a[href="{}"]'.format(settings.ORDERS)).click()
+        # driver.get(ORDERS) # This could replace the line above
+
+        data = driver.page_source
+        
+        soup = BS(data,'lxml')
+        table = soup.find_all('table')[0]
+
+        with io.StringIO() as fh:
+            fh.write('<div>Submit ID; {}</div>'.format(submit_id))
+            fh.write(data)
+            path = default_storage.save('results.html', ContentFile(fh.getvalue().encode('utf-8')))
+
+        return
+
 ##        df = pd.read_html(str(table))[0]
 ##        last_column = df.columns[len(df.columns)-1]
 ##        df = df.drop([0], axis=0) \
