e935585a16dc9b28369e3138251751cfead1d535
johnedstone <johnedstone@gmail.com>
2018-12-20 01:04:12 -0500

    adding a splash page, and using template inheritance

--- mw/urls.py
+++ mw/urls.py
@@ -1,18 +1,15 @@
 from django.urls import path, re_path
 from django.http import HttpResponse
+from django.views.generic.base import RedirectView
 
 from . import views
 from .harvest import harvest
 from .fun_with_storage import fun_with_storage
 
 urlpatterns = [
-    # re_path('^$', lambda request:HttpResponse(
-    # '<div style="margin:2em;color:#005dab;'
-    # 'font-style:italic;font-family:Tahoma,Verdana,sans-serif;">'
-    # 'Palindrome: Do Geese See God?</div>'
-    # )),
-    # path('', views.simple, name='simple'),
-    path('', harvest, name='harvest'),
+    path('',  RedirectView.as_view(pattern_name='splash', permanent=False)),
+    path('splash/', views.splash, name='splash'),
+    path('harvest/', harvest, name='harvest'),
     path('plain/', harvest, {'fancy': False}, name='plain'),
     path('more/', views.simple, name='more'),
     path('fun-with-storage/', fun_with_storage, name='fun_with_storage'),
--- mw/views.py
+++ mw/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.conf import settings
+from django.shortcuts import render
 
 
 def simple(request):
     return HttpResponse("Hello {}".format(settings.USER[:2]))
+
+def splash(request):
+
+    context = {}
+
+    return render(request, 'mw/splash.html', context)
